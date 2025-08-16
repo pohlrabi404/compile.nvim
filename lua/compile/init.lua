371b8173ab155ab43f959cfe7cdc0392ec721e9d
Pohlrabi <pohlrabi404@gmail.com>
2025-08-17 08:28:26 +0900

    feat(compile): allow flexibility for compile()

--- lua/compile/init.lua
+++ lua/compile/init.lua
@@ -124,7 +124,8 @@ function M.clear_hl()
 end
 
 ---Compile project and capture errors
-function M.compile()
+function M.compile(cmd)
+	cmd = cmd or M.opts.cmds.default
 	M.utils.enter_wrapper(function()
 		M.term.destroy()
 		if M.highlight.has_warnings() then
@@ -132,7 +133,7 @@ function M.compile()
 		end
 		M.term.show()
 		M.term.attach_event()
-		M.term.send_cmd(M.opts.cmds.default)
+		M.term.send_cmd(cmd)
 	end)
 end
 
