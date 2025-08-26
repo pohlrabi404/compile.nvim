3f8ec36f0e88b3e7d4b8f34ead0c37adf3235e97
Pohlrabi <pohlrabi404@gmail.com>
2025-08-26 09:27:00 +0900

    fix(float): respect float configuration option

--- lua/compile/init.lua
+++ lua/compile/init.lua
@@ -255,6 +255,11 @@ function M.setup(opts)
 	opts = opts or {}
 	M.opts = vim.tbl_deep_extend("force", M.opts, opts)
 
+	---Make sure to not have split if relative is declared in config
+	if M.opts.term_win_opts.relative ~= nil then
+		M.opts.term_win_opts.split = nil
+	end
+
 	-- Convert relative heights to absolute
 	if M.opts.term_win_opts.height <= 1 then
 		M.opts.term_win_opts.height = math.floor(vim.o.lines * M.opts.term_win_opts.height)
